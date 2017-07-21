8fdef402bae2c75580b9932412a8eea1a441c908
Bernd Toepfer <bernd@bc-toepfer.de>
2017-07-21 13:04:55 +0200

    Serializer angepasst

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,5 +1,5 @@
 class EmployeeSerializer < ActiveModel::Serializer
   attributes :empno, :ename, :job, :hiredate, :sal, :comm
-  #belongs_to :department
+  belongs_to :department
   link(:self)  { department_employee_url(object.department_id, object.id)}
 end
