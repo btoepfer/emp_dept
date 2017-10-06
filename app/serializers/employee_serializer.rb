1b3797db528dbb4047bd4da5404495e0daa3c4fc
Bernd Toepfer <bernd@bc-toepfer.de>
2017-10-06 15:58:53 +0200

    Managerfunktion

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,7 +1,11 @@
 class EmployeeSerializer < ActiveModel::Serializer
   attributes :empno, :ename, :job, :hiredate, :sal, :mgr
   belongs_to :department
-  has_many   :subordinates
+
+  has_many :subordinates, include_data: true
+  
+  
   belongs_to :manager
+
   link(:self)  { department_employee_url(object.department_id, object.id)}
 end
